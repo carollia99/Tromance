e793d2dccf9e88cdd5484e86d4bbf19598c5e38a
Henry Gu <henrygu1722@gmail.com>
2020-11-15 18:52:05 -0800

    fixed register endpoint

--- development/api/models/User.js
+++ development/api/models/User.js
@@ -2,29 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // Create Schema
 const UserSchema = new Schema({
-    info: {
-        type: Object,
-        first: {
-            type: String,
-        },
-        last: {
-            type: String,
-        },
-        age: {
-            type: Number,
-        },
-        personality: {
-            type: String,
-        },
-        bio: {
-            type: String,
-        }
+    first: {
+        type: String,
+    },
+    last: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+    personality: {
+        type: String,
+    },
+    bio: {
+        type: String,
     },
     username: {
         type: String,
     },
     password: {
         type: String,
+    },
+    profilePicture: {
+        type: String,
     }
-});
-module.exports = User = mongoose.model("Users", UserSchema);
+}, { collection: 'Users' });
+module.exports = mongoose.model("Users", UserSchema);
--- development/api/routes/api/users.js
+++ development/api/routes/api/users.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+
+// connect to mongodb through mongoose
+const mongoose = require('mongoose');
+const password = 'Carol1234';
+const db_uri = `mongodb+srv://carol:${password}@cluster0.8rd9p.mongodb.net/Tromance-app?retryWrites=true&w=majority`;
+mongoose.connect(db_uri, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database through mongoose'))
+
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
@@ -11,34 +21,65 @@ const User = require("../../models/User");
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     // Form validation
     const { errors, isValid } = validateRegisterInput(req.body);
     // Check validation
     if (!isValid) {
         return res.status(400).json(errors);
     }
-    User.findOne({ username: req.body.username }).then(user => {
-        if (user) {
-            return res.status(400).json({ username: "Email already exists" });
-        } else {
-            const newUser = new User({
-                username: req.body.username,
-                password: req.body.password
-            });
-            // Hash password before saving in database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser
-                        .save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err));
-                });
-            });
+    console.log('searching for user...');
+    // User.findOne({ "username": req.body.username }).then(user => {
+    //     console.log(user);
+    //     console.log("hi");
+    //     if (user) {
+    //         return res.status(400).json({ username: "Email already exists" });
+    //     } else {
+    //         const newUser = new User({
+    //             username: req.body.username,
+    //             password: req.body.password
+    //         });
+    //         // Hash password before saving in database
+    //         bcrypt.genSalt(10, (err, salt) => {
+    //             bcrypt.hash(newUser.password, salt, (err, hash) => {
+    //                 if (err) throw err;
+    //                 newUser.password = hash;
+    //                 newUser
+    //                     .save()
+    //                     .then(user => res.json(user))
+    //                     .catch(err => console.log(err));
+    //             });
+    //         });
+    //     }
+    // });
+    try {
+        console.log(req.body.username);
+        user = await User.findOne({ 'username': req.body.username });
+        console.log(user);
+        if (user != null) {
+          return res.status(404).json({ message: 'Email already exists' })
         }
-    });
+
+        const newUser = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        // Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+            });
+        });
+
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+    return res.status(200).json({"Success": "True"});
 });
 // @route POST api/users/login
 // @desc Login user and return JWT token
